c336fe24d489f356adeec275bb8a0a0a033557c5
Molly O'Connor <moconnor@carroll.edu>
2026-01-13 14:21:48 -0700

    Added wordlist and function for reading file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def read_word_file(filename):
+    word_list = []
+    try:
+        with open(filename) as filename:
+            for line in filename:
+                line = line.strip()
+                word_list.append(line)
+    except FileNotFoundError:
+        print("File not found")
+    print(word_list)
+    return word_list
+
 
 if __name__ == '__main__':
-    print_hi('Nate')
+    read_word_file('words.txt')
